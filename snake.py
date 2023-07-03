cf48a823289184f597a3f0902476e3eb2acaab92
Shraddha jadav <95160083+JadavShraddha@users.noreply.github.com>
2023-07-03 22:05:27 +0530

    Update snake.py

--- snake.py
+++ snake.py
@@ -2,7 +2,11 @@
 # 1.Installing Pygame
 # 2.Create the Screen
 # 3.Create the Snake
-
+# 4.Moving the Snake
+# 5.Game Over when Snake hits the boundaries
+# 6.Adding the Food
+# 7.Increasing the Length of the Snake
+# 8.Displaying the Score
 
 
 import pygame
